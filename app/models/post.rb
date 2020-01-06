aa608226a572c796852fbedbaf11dcc3431772bf
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2020-01-06 20:40:22 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,7 +15,7 @@ end
 
 
 # class Clickbait < ActiveModel::Validator
-  def validate(record)
-    unless record.title != "Won't Believe" || "Secret" || "Top [number]" || "Guess"
-  end 
+  # def validate(record)
+  #   unless record.title != "Won't Believe" || "Secret" || "Top [number]" || "Guess"
+  # end 
 # end 
