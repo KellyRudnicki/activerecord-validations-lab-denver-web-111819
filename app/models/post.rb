578883a020e56996a8cc442cde027907061651ef
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2020-01-06 20:24:21 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :summary, length: {maximum: 250} 
   validates :content, length: {minimum: 250}
-  validates :category, include
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid size" }
 end
