879c33990c2682234e5269637528fdad02665594
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2020-01-06 20:20:35 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :summary, length: { minimum: 250}
-  validates :content, length: { is: 10} 
+  validates :content, length: { maximum: 250} 
 end
