2250aeb8221adec26f9beb544bfcab9d07553f5e
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2020-01-06 21:13:39 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,25 @@
 class Post < ActiveRecord::Base
   
-  include ActiveModel::Validations
-  validates_with TitleValidator
+  # include ActiveModel::Validations
+  # validates_with TitleValidator
   
-  validates :title, presence: true
+  validates :title, presence: true, title: true
   validates :summary, length: {maximum: 250} 
   validates :content, length: {minimum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
     message: "%{value} is not a valid category" }
   
+end
+
+
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+ 
+class Person < ApplicationRecord
+  validates :email, presence: true, email: true
 end
