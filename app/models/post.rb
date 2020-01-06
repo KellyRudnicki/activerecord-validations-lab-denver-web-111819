071e34e7a4a8943120d9c5ecb8fa558e800f10a1
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2020-01-06 21:13:52 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,16 +10,3 @@ class Post < ActiveRecord::Base
     message: "%{value} is not a valid category" }
   
 end
-
-
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "is not an email")
-    end
-  end
-end
- 
-class Person < ApplicationRecord
-  validates :email, presence: true, email: true
-end
