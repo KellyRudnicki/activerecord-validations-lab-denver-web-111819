5797a8499aaad3409b2f9e4c1508f390b1c5e707
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2020-01-06 21:11:09 +0000

    Automatically backed up by Learn

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,7 +1,7 @@
 class TitleValidator < ActiveModel::Validator
   
   def validate(record)
-      unless !record.title.include? ("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+      unless !record.title.include? ("Won't Believe" || "Secret" || "Top #" || "Guess")
       record.errors[:title] << 'Need a clickbaity title!'
     end
   end 
