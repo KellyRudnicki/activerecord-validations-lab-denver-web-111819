26bfac3cc12ac729e292b00781557132b0735a7c
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2020-01-06 21:09:37 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   
-  validates :title, presence: true
+  # validates :title, presence: true
   validates :summary, length: {maximum: 250} 
   validates :content, length: {minimum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
