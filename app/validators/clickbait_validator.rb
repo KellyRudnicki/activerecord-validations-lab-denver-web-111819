ee2418ccd42ce352445282224e8245f1d2f239ce
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2020-01-06 21:33:37 +0000

    Automatically backed up by Learn

--- app/validators/clickbait_validator.rb
+++ app/validators/clickbait_validator.rb
@@ -1,4 +1,4 @@
-class ClickbaitValidator < ActiveModel::Validator
+class ClickbaitValidator < ActiveModel::EachValidator
   # def validate_each(record, attribute, value)
   #     unless value =~ /(Won't Believe|Secret|Top \d|Guess)/
   #     record.errors[attribute] << ("Need a clickbaity title!")
