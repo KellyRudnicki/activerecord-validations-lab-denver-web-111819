33b12210c8d49b4c64b06768be7895fc10ff4dac
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2020-01-06 20:51:04 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,11 @@
 class Post < ActiveRecord::Base
+  include 
   validates :title, presence: true
   validates :summary, length: {maximum: 250} 
   validates :content, length: {minimum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
     message: "%{value} is not a valid category" }
     
-  validates clickbait
-  
-  def clickbait(record)
-    unless record.title != "Won't Believe" || "Secret" || "Top [number]" || "Guess"
-  end 
+  validates 
   
 end
