59f560a8a83e454be376dc0129ff221c11e4a080
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2020-01-06 21:19:34 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,5 +8,6 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
     message: "%{value} is not a valid category" }
+  validates :title, clickbait: true
   
 end
