0318e0ce202276c9f41297ef8ac8da3221998e26
Kelly Rudnicki <kelly.rudnicki@comcast.net>
2020-01-06 20:41:36 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
     message: "%{value} is not a valid category" }
     
-  validates clickbait, on: :create 
+  validates clickbait
   
   def clickbait(record)
     unless record.title != "Won't Believe" || "Secret" || "Top [number]" || "Guess"
